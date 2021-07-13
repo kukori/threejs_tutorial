88bc31c7488a3d5ad48d95a994203c070a366164
kukori <attila.varjas@gmail.com>
2021-07-14 06:43:27 +0700

    l7 change camera movement

--- l7/src/script.js
+++ l7/src/script.js
@@ -65,7 +65,10 @@ const tick = () =>
     // Update objects
     // mesh.rotation.y = elapsedTime;
 
-    camera.position.x = cursor.x * 3
+    // camera.position.x = cursor.x * 3
+    // camera.position.y = cursor.y * 3
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
     camera.position.y = cursor.y * 3
     camera.lookAt(mesh.position)
 
