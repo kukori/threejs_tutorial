29dae66ef391573f229307269952fd08c3dae0d9
kukori <attila.varjas@gmail.com>
2021-07-14 07:19:38 +0700

    l7 change camera control to OrbitControls

--- l7/src/script.js
+++ l7/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Cursor
 const cursor = {
@@ -49,6 +50,12 @@ camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -65,12 +72,13 @@ const tick = () =>
     // Update objects
     // mesh.rotation.y = elapsedTime;
 
-    // camera.position.x = cursor.x * 3
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
     // camera.position.y = cursor.y * 3
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 3
-    camera.lookAt(mesh.position)
+    // camera.lookAt(mesh.position)
+
+    // Update controls here because of the damping
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
