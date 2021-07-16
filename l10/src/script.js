8bd4faf48d92b62aa86cf9318e39fac4eb5bf7cd
kukori <attila.varjas@gmail.com>
2021-07-16 19:10:17 +0700

    l10

---
+++ l10/src/script.js
@@ -0,0 +1,104 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+// Debug 
+const gui = new dat.GUI({ closed: true })
+const debugParameters = {
+    color: '#00dfad',
+    spin: () => {
+        gsap.to(mesh.rotation, { duration: 1,   y: mesh.rotation.y + Math.PI * 2 })
+    }
+}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height =  window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}) 
+
+window.addEventListener('dblclick', () => {
+    if(!document.fullscreenElement) {
+        canvas.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+}) 
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: debugParameters.color})
+)
+scene.add(mesh)
+
+// gui.add(mesh.position, 'y', -3, 3, 0.01)
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01)
+gui.add(mesh.position, 'x', -3, 3, 0.01)
+gui.add(mesh.position, 'z', -3, 3, 0.01)
+
+gui.add(mesh, 'visible')
+gui.add(mesh.material, 'wireframe')
+gui.addColor(debugParameters, 'color').onChange(() =>{
+    mesh.material.color.set(debugParameters.color)
+})
+
+gui.add(debugParameters, 'spin')
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+// to prevent perf issues we limit the PR to max 2
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
