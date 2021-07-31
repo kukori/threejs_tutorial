83acaee96cc632fe000e8fe93849c919ea706bc4
kukori <attila.varjas@gmail.com>
2021-07-31 15:40:20 +0200

    L12 materials

--- l12/src/script.js
+++ l12/src/script.js
@@ -1,7 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { BackSide, MeshStandardMaterial } from 'three'
+import * as dat from 'dat.gui'
 
+// Debug
+const gui = new dat.GUI()
+  
 const LoadingManager = new THREE.LoadingManager()
 const textureLoader = new THREE.TextureLoader(LoadingManager)
 const cubeTextureLoader = new THREE.CubeTextureLoader(LoadingManager)
@@ -13,9 +18,14 @@ const doorHeightTexture = textureLoader.load('/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/door/roughness.jpg')
-const matcapTexture = textureLoader.load('/matcaps/1.png')
+const matcapTexture = textureLoader.load('/matcaps/2.png')
 const gradientTexture = textureLoader.load('/gradients/3.jpg')
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
 
+// more env maps: https://polyhaven.com/
+// convert to cubeview: https://matheowis.github.io/HDRI-to-CubeMap/
 const environmentMapTexture = cubeTextureLoader.load([
     '/environmentMaps/0/px.jpg',
     '/environmentMaps/0/nx.jpg',
@@ -35,25 +45,87 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-const material = new THREE.MeshBasicMaterial({ map: doorColorTexture })
+// const material = new THREE.MeshBasicMaterial({ map: doorColorTexture })
+// const material = new THREE.MeshBasi cMaterial()
+// material.map = doorColorTexture
+// material.color = new THREE.Color('#00dfad')
+// material.opacity = 0.5
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide // (BackSide, FrontSide)
+ 
+// const material = new THREE.MeshNormalMaterial()
+// material.flatShading = true
+
+// you can simulate lights with matcaps https://github.com/nidorx/matcaps
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// const material = new THREE.MeshDepthMaterial()
+
+// const material = new THREE.MeshLambertMaterial()
+
+// less performant than lambert
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 100
+// material.specular = new THREE.Color('#1188ff')
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+// const material = new MeshStandardMaterial()
+// // material.metalness = 0.45
+// // material.roughness = 0.65
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = 
+// material.normalMap = doorNormalTexture
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+const material = new MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(1).step(0.0001)
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.SphereBufferGeometry(0.5, 64, 64),
     material
 )
-sphere.position.x = 1.5
+sphere.position.x = 1.
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(1, 1),
+    new THREE.PlaneBufferGeometry(1, 1, 100, 100),
     material
 )
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
 
 const torus = new THREE.Mesh(
-    new THREE.TorusBufferGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusBufferGeometry(0.3, 0.2, 64, 128),
     material
 )
 torus.position.x = -1.5
+torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
 scene.add(sphere, plane, torus)
+
+// Lights
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight('#ffffff', 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 // Sizes
 const sizes = {
